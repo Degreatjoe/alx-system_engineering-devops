6eacbf45d0271bd2cb44117a7d8713bbb9445b66
great <jgreat770@gmail.com>
2024-06-29 07:36:34 +0100

    task1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,11 @@
-# This Puppet manifest installs Flask version 2.1.0 using pip3
+# This Puppet manifest installs Flask version 2.1.0 and Werkzeug version 2.1.1 using pip3
 package { 'python3-pip':
   ensure => 'present',
 }
 
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
+exec { 'install_flask_and_werkzeug':
+  command => '/usr/bin/pip3 install flask==2.1.0 werkzeug==2.1.1',
   path    => '/usr/bin/',
-  unless  => '/usr/bin/pip3 show flask | grep -q "^Version: 2.1.0"',
+  unless  => '/usr/bin/pip3 show flask | grep -q "^Version: 2.1.0" && /usr/bin/pip3 show werkzeug | grep -q "^Version: 2.1.1"',
   require => Package['python3-pip'],
 }
