035895a11db1f7062e38f08e2535b5dd0edaba35
Great~ <jgreat779@gmail.com>
2024-07-01 10:50:01 +0100

    good

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,19 +1,14 @@
 # This Puppet manifest configures the SSH client to use the private key ~/.ssh/school
 # and refuse password authentication.
 
-$home = $facts['os']['family'] ? {
-  'Darwin' => $facts['env']['HOME'],
-  'default' => "/home/${facts['identity']['user']}"
-}
-
-file { "${home}/.ssh/config":
+file { '.ssh/config':
   ensure  => 'file',
-  owner   => $facts['identity']['user'],
-  group   => $facts['identity']['group'],
+  owner   => 'degreatjoe',
+  group   => 'degreatjoe',
   mode    => '0600',
-  content => @(EOF)
+  content => @("EOF")
     Host *
-      IdentityFile ${home}/.ssh/school
+      IdentityFile .ssh/school
       PasswordAuthentication no
     | EOF
 }
