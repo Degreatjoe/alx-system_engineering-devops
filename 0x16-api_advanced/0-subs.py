689bf55590c19e49c7f64c192f312b56613196df
great_joseph <jgreat7670@gmail.com>
2024-08-15 02:57:02 +0100

    api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,13 +6,16 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Opera"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    """
+    Returns the number of subscribers for a given subreddit.
+    If the subreddit is invalid, returns 0.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    # headers = {"User-Agent": "My Reddit API Client"}  # Set a custom User-Agent
+    response = requests.get(url, allow_redirects=False)
+
     if response.status_code == 200:
         data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
+        return data["data"]["subscribers"]
     else:
         return 0
